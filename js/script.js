f422df2065afc96299b8879b46b2d26eaf3ee657
Ben Ma <git.app@benjaminma.com>
2012-02-12 21:51:28 -0800

    call generateLazypass() on input key press

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 window.onload = init;
 
 function init() {
-//	alert("script.js - init() - window.onload")		
+	var isk = $("#input-secret-key");
+	isk.keyup(isk_KeyUp);
+}
+
+function isk_KeyUp() {
+	var seed = $("#input-secret-key").val();
+	generateLazypass(seed);
+
+	// TODO: Test for input delay/intent
+}
+
+function generateLazypass(seed) {
+	document.write("generateLazypass()");
+	// TODO: Select character set
+	// TODO: Filter user options
+	// TODO: Seed PRNG
+	// TODO: Generate random numbers
+	// TODO: Build lazypass table
 }
